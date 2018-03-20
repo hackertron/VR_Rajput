3e71b536d5827c7d7ac2760c52cde148fa8575af
jayssj11 <cooljay.gupta@gmail.com>
2018-03-21 04:59:19 +0530

    Create raja.cpp

---
+++ Source/raja/raja.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "cpp_park.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, cpp_park, "raja" );
+ 
